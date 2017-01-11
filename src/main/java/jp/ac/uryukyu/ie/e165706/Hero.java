e087de8ecca543fd18c9f06a46deecf0a1564293
Yamato Goto <e165706@ie.u-ryukyu.ac.jp>
2017-01-11 23:35:13 +0900

    report7 Hero escape method

--- src/main/java/jp/ac/uryukyu/ie/e165706/Hero.java
+++ src/main/java/jp/ac/uryukyu/ie/e165706/Hero.java
@@ -1,4 +1,5 @@
 package jp.ac.uryukyu.ie.e165706;
+import java.util.Scanner;
 
 public class Hero extends LivingThing {
     public Hero(String name, int hitPoint, int attack) {
@@ -7,19 +8,43 @@ public class Hero extends LivingThing {
 
     @Override
     public int attack_option(int damage, LivingThing opponent) {
-        if (damage == 0) {
-            System.out.printf("%sの攻撃！...だが、%sは攻撃を回避した！\n", getName(), opponent.getName());
-        }
-        else{
-            double hit =Math.random() *100;
-            if (hit >= 70){
-                damage = damage *2;
-                System.out.printf("%sの攻撃！会心の一撃！！%sに%sのダメージを与えた！！\n",getName(),opponent.getName(),damage);
+        System.out.println("戦う:1/逃げる:2\nコマンドを入力してください。:");
+        Scanner sc = new Scanner(System.in);
+        int comand = sc.nextInt();
+        if (comand == 1) {
+            if (damage == 0) {
+                System.out.printf("%sの攻撃！...だが、%sは攻撃を回避した！\n", getName(), opponent.getName());
+            } else {
+                double hit = Math.random() * 100;
+                if (hit >= 70) {
+                    damage = damage * 2;
+                    System.out.printf("%sの攻撃！会心の一撃！！%sに%sのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                } else {
+                    System.out.printf("%sに%dのダメージを与えた！！\n", opponent.getName(), damage);
+                }
             }
-            else{
-                System.out.printf("%sに%dのダメージを与えた！！\n", opponent.getName(), damage);
-            }
+            return damage;
+        } else if (comand == 2) {
+
+            escape(opponent.getName());
+            return 0;
+        } else {
+            attack_option(damage,opponent);
         }
-        return damage;
+
+    return 0;
     }
+
+    public static void escape(String monster) {
+        double value = Math.random() * 100;
+        if (monster == "スライム") {
+            if (value >=70) {
+                System.out.println("勇者は逃げ出した!!");
+            } else {
+                System.out.println("勇者は逃げようとしたが,回り込まれてしまった!!");
+            }
+        }
+    }
+
+
 }
